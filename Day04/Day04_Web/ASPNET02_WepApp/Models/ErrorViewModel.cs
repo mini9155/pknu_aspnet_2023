5f130c7951151ad6988ed7f96c9aaf54de2d4dc7
mimi9155 <hanbv@naver.com>
2023-05-23 09:14:37 +0900

    -

--- Day04/Day04_Web/ASPNET02_WepApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASPNET02_WepApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ Day04/aspnet01_webapp/aspnet02_boardapp/Models/Board.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace aspnet02_boardapp.Models
+{
+    // 게시판을 위한 테이블 매핑 정보
+    public class Board
+    {
+        [Key]   // PK
+        public int Id { get; set; }
+
+        [Required]  // Not Null
+        [DisplayName("아이디")]
+        
+        public string UserId { get; set; }
+        [DisplayName("이름")]
+
+        public string? UserName { get; set; }   // ?를 붙이면 Null 허용
+
+        [Required]  // Not Null
+        [MaxLength(200)]    // Varchar(200)이랑 같음
+        
+        [DisplayName("제목")]
+        public string Title { get; set; }
+        
+        [DisplayName("조회")]
+        public string ReadCount { get; set; }
+
+        [DisplayName("작성일")]
+        public DateTime PostDate { get; set; }
+       
+        [DisplayName("내용")]
+        public string Contents { get; set; }
+    }
+}
